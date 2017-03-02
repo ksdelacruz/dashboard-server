7858b5a29014fde619b4ae54c4b1eba7522b7468
Kevin Dhale dela Cruz <kevindhaledelacruz@gmail.com>
2017-03-02 12:27:18 +0800

    - Initial commit

---
+++ bin/dashboard-server.php
@@ -0,0 +1,27 @@
+<?php
+
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use React\Socket\Server as Reactor;
+use MyApp\Dashboard;
+use React\EventLoop\Factory as LoopFactory;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+// $loop = LoopFactory::create();
+// $server = IoServer::factory( new HttpServer(new WsServer(new Dashboard($loop) ) ), 8080);
+
+// // $server->loop->addPeriodicTimer(5, function () use ($server) {        
+// //     echo "LOOPING";
+// // });
+
+// $server->run();
+
+$loop = LoopFactory::create();
+$socket = new Reactor($loop);
+$socket->listen(999, '0.0.0.0');
+$server = new IoServer(new HttpServer(new WsServer(new Dashboard($loop))), $socket, $loop);
+$server->run();
+
+?>
---
+++ src/MyApp/Dashboard.php
@@ -0,0 +1,84 @@
+<?php
+namespace MyApp;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use React\EventLoop\StreamSelectLoop as Loop;
+
+class Dashboard implements MessageComponentInterface 
+{
+	protected $clients;
+
+	public function __construct(Loop $loop) {
+	    $this->clients = new \SplObjectStorage;
+	    echo "Congratulations! the server is now running\n";
+
+        $loop->addPeriodicTimer(60, function() {
+            // Loop every 1 minute to get if latest JSON exists
+            foreach ($this->clients as $client) {
+               $this->getJSON($client);
+            }
+            echo "Looping every 1 minute...\n";
+        });
+	}
+
+    public function getJSON($client) {
+        $host = $client->WebSocket->request->getHeader('Origin');
+        $json = file_get_contents($host . '/temp/data/PublicAlert.json');
+        $data = array(
+            'code' => 'getJSONandLastRelease',
+            'alert_json' => json_decode($json),
+        );
+        if($json === FALSE) $data['is_bad'] = "ERROR: Problem with opening JSON";
+
+        $client->send(json_encode($data));
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+    	// Store the new connection to send messages to later
+	    $this->clients->attach($conn);
+
+        // Send JSON after connecting
+        $this->getJSON($conn);
+
+	    echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+    	$numRecv = count($this->clients) - 1;
+    	echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n", $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+        $host = $from->WebSocket->request->getHeader('Origin');
+
+        if($msg == "getOnGoingAndExtended") {
+            $ongoing = file_get_contents($host . '/monitoring/getOnGoingAndExtended');
+            $data = array(
+                'code' => 'getOnGoingAndExtended',
+                'ongoing' => json_decode($ongoing)
+            );
+
+            foreach ($this->clients as $client) {
+                $client->send(json_encode($data));
+            }
+        }
+
+    	// foreach ($this->clients as $client) {
+	    //     if ($from !== $client) {
+	    //         // The sender is not the receiver, send to each client connected
+	    //         $client->send($msg);
+	    //     }
+	    // }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+    	// The connection is closed, remove it, as we can no longer send it messages
+    	$this->clients->detach($conn);
+
+    	echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+    	echo "An error has occurred: {$e->getMessage()}\n";
+
+    	$conn->close();
+    }
+}
